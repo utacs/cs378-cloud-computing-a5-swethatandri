ba355bdb796191bbb3d83cfa3ed2144ab130634a
swethatandri <65615296+swethatandri@users.noreply.github.com>
2024-07-05 02:12:36 -0500

    Update GradientMapperTask3.java

--- src/main/java/edu/cs/utexas/HadoopEx/GradientMapperTask3.java
+++ src/main/java/edu/cs/utexas/HadoopEx/GradientMapperTask3.java
@@ -9,11 +9,11 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class GradientMapperTask3 extends Mapper<LongWritable, Text, Text, DoubleWritable> {
-    private double w0;
-    private double w1;
-    private double w2;
-    private double w3;
-    private double w4;
+    private double m1;
+    private double m2;
+    private double m3;
+    private double m4;
+    private double b;
 
     private Text MapKey = new Text();
     private DoubleWritable MapValue = new DoubleWritable();
@@ -21,32 +21,54 @@ public class GradientMapperTask3 extends Mapper<LongWritable, Text, Text, Double
     @Override
     public void setup(Context context) {
         Configuration conf = context.getConfiguration();
-        w0 = Double.parseDouble(conf.get("w0"));
-        w1 = Double.parseDouble(conf.get("w1"));
-        w2 = Double.parseDouble(conf.get("w2"));
-        w3 = Double.parseDouble(conf.get("w3"));
-        w4 = Double.parseDouble(conf.get("w4"));
+        m1 = Double.parseDouble(conf.get("m1"));
+        m2 = Double.parseDouble(conf.get("m2"));
+        m3 = Double.parseDouble(conf.get("m3"));
+        m4 = Double.parseDouble(conf.get("m4"));
+        b = Double.parseDouble(conf.get("b"));
     }
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] fields = value.toString().split(",");
         if (cleanUpData(fields)) {
-            double x0 = 1.0; // Bias term
+            //double x0 = 1.0; // Bias term
             double x1 = Double.parseDouble(fields[4]); // Trip time in seconds
             double x2 = Double.parseDouble(fields[5]); // Trip distance in miles
             double x3 = Double.parseDouble(fields[11]); // Fare amount in dollars
             double x4 = Double.parseDouble(fields[15]); // Tolls amount in dollars
             double y = Double.parseDouble(fields[16]); // Total amount paid in dollars
 
-            double prediction = w0 * x0 + w1 * x1 + w2 * x2 + w3 * x3 + w4 * x4;
+            double prediction = m1 * x1 + m2 * x2 + m3 * x3 + m4 * x4 + b;
             double error = y - prediction;
 
-            context.write(new Text("w0Gradient"), new DoubleWritable(-2 * x0 * error));
-            context.write(new Text("w1Gradient"), new DoubleWritable(-2 * x1 * error));
-            context.write(new Text("w2Gradient"), new DoubleWritable(-2 * x2 * error));
-            context.write(new Text("w3Gradient"), new DoubleWritable(-2 * x3 * error));
-            context.write(new Text("w4Gradient"), new DoubleWritable(-2 * x4 * error));
+            MapKey.set("m1Gradient");
+            MapValue.set(-x1 * error);
+            context.write(MapKey, MapValue);
+
+            MapKey.set("m2Gradient");
+            MapValue.set(-x2 * error);
+            context.write(MapKey, MapValue);
+
+            MapKey.set("m3Gradient");
+            MapValue.set(-x3 * error);
+            context.write(MapKey, MapValue);
+
+            MapKey.set("m4Gradient");
+            MapValue.set(-x4 * error);
+            context.write(MapKey, MapValue);
+
+            MapKey.set("bGradient");
+            MapValue.set(-1 * error);
+            context.write(MapKey, MapValue);
+
+            MapKey.set("cost");
+            MapValue.set(error * error);
+            context.write(MapKey, MapValue);
+
+            MapKey.set("COUNT");
+            MapValue.set(1.0);
+            context.write(MapKey, MapValue);
         }
     }
 
